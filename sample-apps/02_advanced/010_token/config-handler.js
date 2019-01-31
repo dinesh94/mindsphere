567f5b36c287e9d4623b132165149820cd44a64a
Dinesh <dinesh.bhavsar@siemens.com>
2019-01-31 12:34:39 +0530

    renamed folder

--- sample-apps/02_advanced/010_token/config-handler.js
+++
@@ -1,3 +0,0 @@
-module.exports = function(req, res) {
-    res.end('Config - you are authorized to see this :-) !!');
-}
--- sample-apps/02_advanced/010_token/status-handler.js
+++
@@ -1,3 +0,0 @@
-module.exports = function statusHandler(req, res) {
-    res.end('Status');
-}
